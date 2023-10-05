85de6f0ca66a2a29473e3a58866996c1562a27a3
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2023-10-05 14:32:43 +0100

    feat(nvim): remove current line ending

--- nvim/.config/nvim/lua/keymaps/commonedits.lua
+++ nvim/.config/nvim/lua/keymaps/commonedits.lua
@@ -4,3 +4,6 @@ local nmap = require("keymaps.mapping").nmap
 nmap("<leader>a,", "mzA,<esc>`z", "[a]ppend [,]")
 nmap("<leader>a.", "mzA.<esc>`z", "[a]ppend [,]")
 nmap("<leader>a;", "mzA;<esc>`z", "[a]ppend [,]")
+
+-- Remove the current line ending
+nmap("<leader>ax", "mz$x<esc>`z", "[a]ppend [x]")
